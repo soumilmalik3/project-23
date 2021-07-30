644a0c16ef829a9f987493d5df4ec9c92fcfd733
soumilmalik3 <80881629+soumilmalik3@users.noreply.github.com>
2021-07-30 17:37:28 +0530

    Update box.js

--- box.js
+++ box.js
@@ -3,7 +3,8 @@ class Box {
     var options = {
         'restitution':0.8,
         'friction':0.3,
-        'density':1.0
+        'density':1.0,
+        'isStatic':True;
     }
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
